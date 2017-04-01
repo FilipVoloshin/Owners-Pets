5aa93720e923e33be05da46507119f013856d912
A09F\filip <filippvoloshin@gmail.com>
2017-04-01 19:00:57 +0300

    added DBHelper class with logic to add and delete information from tables in database

--- Owners-Pets/Controllers/OwnersController.cs
+++ Owners-Pets/Controllers/OwnersController.cs
@@ -5,11 +5,18 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Owners_Pets.Models;
+using Owners_Pets.Helpers;
 
 namespace Owners_Pets.Controllers
 {
+    
     public class OwnershipsController : ApiController
     {
-        
+        public IEnumerable<string> Get()
+        {
+            DBHelper.StartConnection();
+            DBHelper.DeleteOwner(3);
+            return new string[] { "value1", "value2" };
+        }
     }
 }
---
+++ Owners-Pets/Helpers/DBHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace Owners_Pets.Helpers
+{
+    public static class DBHelper
+    {
+        private static SQLiteConnection _dbConnection;
+        private static SQLiteCommand _command;
+
+        /// <summary>
+        /// Starts the connection whith DataBase
+        /// </summary>
+        public static void StartConnection()
+        {
+            _dbConnection = new SQLiteConnection(@"Data Source=C:\git_root\Owners-Pets\Owners-Pets\Ownerships.db;Version=3;");
+            _dbConnection.Open();
+            
+        }
+        /// <summary>
+        /// Allows to add owner to the Owners table
+        /// </summary>
+        /// <param name="name"></param>
+        public static void AddOwner(string name)
+        {
+            string sql = $"insert into Owners ('Name') values ('{name}')";
+            _command = new SQLiteCommand(sql, _dbConnection);
+             _command.ExecuteNonQuery();
+        }
+        /// <summary>
+        /// Allows to add pet to the pets table
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="ownerId"></param>
+        public static void AddPet(string name,int ownerId)
+        {
+            string sql = $"insert into Pets ('Name','OwnerId') values ('{name}','{ownerId}')";
+            _command = new SQLiteCommand(sql, _dbConnection);
+            _command.ExecuteNonQuery();
+        }
+        /// <summary>
+        /// Delete owner by ID
+        /// </summary>
+        /// <param name="id"></param>
+        public static void DeleteOwner(int id)
+        {
+            string sql = $"delete from Owners where ID = {id}";
+            _command = new SQLiteCommand(sql, _dbConnection);
+            _command.ExecuteNonQuery();
+        }
+        /// <summary>
+        /// Delete pet by ID
+        /// </summary>
+        /// <param name="id"></param>
+        public static void DeletePet(int id)
+        {
+            string sql = $"delete from Pets where ID = {id}";
+            _command = new SQLiteCommand(sql, _dbConnection);
+            _command.ExecuteNonQuery();
+        }
+
+        //TODO add constraint to the foreign key.
+    }
+
+    //
+}
