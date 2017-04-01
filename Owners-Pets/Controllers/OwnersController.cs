63502ee28d34d2f6d5f2090306a597f15942c898
A09F\filip <filippvoloshin@gmail.com>
2017-04-01 13:29:35 +0300

    added some values to db

---
+++ Owners-Pets/Controllers/OwnersController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Owners_Pets.Models;
+
+namespace Owners_Pets.Controllers
+{
+    public class OwnershipsController : ApiController
+    {
+        
+    }
+}
