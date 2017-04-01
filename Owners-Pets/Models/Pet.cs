66decac6e2ad972568f2f9b0b4bae58cddf9441c
A09F\filip <filippvoloshin@gmail.com>
2017-04-01 12:46:04 +0300

    Added simple model-entities to the  project

---
+++ Owners-Pets/Models/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Owners_Pets.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string PetName { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
