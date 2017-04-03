d1467d011f140aa2f088891f1ff4c04ad2491dce
A09F\filip <filippvoloshin@gmail.com>
2017-04-03 14:37:24 +0300

    created AJAX converting helper and added configuration to WebApiConfig

--- Owners-Pets/App_Start/WebApiConfig.cs
+++ Owners-Pets/App_Start/WebApiConfig.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Owners_Pets.Helpers;
 
 namespace Owners_Pets
 {
+
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             //Convert to JSON format
-            config.Formatters.JsonFormatter.SupportedMediaTypes
-                 .Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.Add(new BrowserJsonFormatter());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
