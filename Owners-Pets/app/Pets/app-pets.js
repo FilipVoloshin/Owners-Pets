312c5020a2d3358ac2bb6b3928a612b57268d98a
A09F\filip <filippvoloshin@gmail.com>
2017-04-05 21:53:44 +0300

    updated view controller

--- Owners-Pets/app/Pets/app-pets.js
+++ Owners-Pets/app/Pets/app-pets.js
@@ -1,1 +1,10 @@
-﻿
+﻿// app-pets.js
+
+(function () {
+
+    "use strict";
+
+    //Creating the module
+    angular.module("app-pets", []);
+
+})(); 
--- Owners-Pets/app/Pets/petsController.js
+++ Owners-Pets/app/Pets/petsController.js
@@ -1,1 +1,35 @@
-﻿
+﻿//ownersController.js
+(function () {
+
+    "use strict";
+
+    //create a controller: (getting the existing module)
+    angular.module("app-pets")
+        .controller("petsController", petsController);
+
+    //$http - call to server
+    function petsController($http) {
+
+        //ViewModel
+        var vm = this;
+
+        //Array of the db information
+        vm.pets = [];
+
+        //Get information from api get request
+        $http.get("api/pets/1")
+            .then(function (response) {
+                //success
+                angular.copy(response.data, vm.owners);
+            }, function (error) {
+                //failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+    };
+
+
+
+}());
